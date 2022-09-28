bd1f26aa93520ed5f5a48259330fdfb404d1d451
Simion <simion.okola@student.moringaschool.com>
2022-09-29 00:27:53 +0300

    Completed assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,25 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  const movieArray = movies.map((movie)=>(
+    <div key={movie.title} >
+      {movie.title}
+      {movie.time}
+      <ul>
+        {movie.genres.map((genre)=>(
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movieArray}
+    </div>
+  )
 }
 
 export default Movies;
+
